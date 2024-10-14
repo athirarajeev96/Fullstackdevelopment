2375bf753535dcafff738b36685ee1c86ca3b6e4
athirarajeev96 <athirabahuleyan96@gmail.com>
2024-10-14 19:10:30 +0530

    final changes - deployed url

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ import './models/index.js';  // Ensure MongoDB connection is established
 const PORT = process.env.PORT || 8000;  // Use the port you've set or default to 8000
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+    origin: 'https://bright-cactus-65cc86.netlify.app', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+    credentials: true, // Enable set cookie
+};
+
 // Middleware
-app.use(cors());  // Allow CORS
+app.use(cors(corsOptions));  // Allow CORS with specified options
 app.use(express.json());  // Parse incoming JSON requests
 
 // Mounting the main router for API routes
@@ -17,11 +24,11 @@ app.use('/api', Routes); // All API routes will now start with /api
 
 // Admin route with middleware
 app.use('/admin', authMiddleware, adminMiddleware, (req, res) => {
-  res.send('Admin Access');  // Responding for admin access
+    res.send('Admin Access');  // Responding for admin access
 });
 
 // Health check route
-app.get('/', (req, res) => res.send('<h1>Server is running</h1>'));  // Simple health check
+app.get('/', (req, res) => res.send('Server is running'));  // Simple health check
 
 // Start the server
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
